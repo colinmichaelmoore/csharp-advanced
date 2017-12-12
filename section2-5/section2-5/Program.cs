19b4c6314207c8cd149c4e0871f7625b373f8bb5
colinmichaelmoore <colinmichaelmoore317@intracitygeeks.org>
2017-12-12 01:01:01 -0500

    Initial advanced C#

---
+++ section2-5/section2-5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace section2_5
+{
+    public class GenericList<T>
+    {
+        public void Add(T value)
+        {
+            
+        }
+
+        public T this[int index]
+        {
+            get {throw new NotImplementedException();}
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var book = new Book {Isbn = "1111", Title = "Advanced C#"};
+
+//            var numbers = new List();
+//            numbers.Add(10);
+//
+//            var books = new BookList();
+//            books.Add(book);
+
+            var numbers = new GenericList<int>();
+            numbers.Add(10);
+
+            //var books = new GenericList<book>();
+            //books.Add(new Book());
+
+        }
+    }
+}
---
+++ section2-7/section2-7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace section2_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var books = new BookRepository().GetBooks();
+
+            //var cheapBooks = books.FindAll(book => book.Price < 10); Lambda expression
+
+            //var cheapBooks = books.FindAll(IsCheaperThan10Dollars);
+            //            foreach (book in cheapBooks)
+            //            {
+            //                Console.WriteLine(book.Title);
+            //            }
+        }
+
+        //        static bool IsCheaperThan10Dollars(Book book)
+        //        {
+        //            return book.Price < 10;
+        //        }
+    }
+}
