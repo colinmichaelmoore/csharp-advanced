6843ae8da00392cdc3ab653fec9b39e3c773c421
colinmichaelmoore <colinmichaelmoore317@intracitygeeks.org>
2017-12-14 22:46:45 -0500

    LINQ

---
+++ advanced-linq/advanced-linq/Book.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+
+namespace advanced_linq
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public float Price { get; set; }
+    }
+}
---
+++ advanced-linq/advanced-linq/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advanced_linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var books = new BookRepository().GetBooks();
+/*
+            //LINQ extension methods
+            books.Single();
+            books.SingleOrDefault();
+
+            books.First();
+            books.FirstOrDefault();
+
+            books.Last();
+            books.LastOrDefault();
+
+            books.Min();
+            books.Max();
+            books.Count();
+            books.Sum();
+            books.Average(b => b.Price);
+
+            books.Skip(5).Take(3);*/
+
+//            var cheapBooks = new List<Book>();
+//            foreach (var book in books)
+//            {
+//                if (book.Price < 10)
+//                    cheapBooks.Add(book);
+//            }
+
+            //LINQ query operators
+
+            /*var cheaperBooks = 
+                from b in books
+                where b.Price < 10
+                orderby b.Title
+                select b.Title;
+
+            //Linq extension methods
+            var cheapBooks = books
+                .Where(b => b.Price < 10)
+                .OrderBy(b => b.Title)
+                .Select(b => b.Title);
+
+            //books.OrderBy(b => b.Title);
+
+            foreach (var book in cheapBooks)
+            {
+                Console.WriteLine(book);
+                //Console.WriteLine(book.Title + " " + book.Price);
+            }*/
+        }
+    }
+}
